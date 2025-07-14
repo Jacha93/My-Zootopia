37c2c3ff43d591fa2e1a39c89f9ea8578f1a2cf4
Jacha_93 <keinplan1993@gmail.com>
2025-07-14 21:11:39 +0200

    Daten aus JSON-Datei auslesen

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -5,4 +5,15 @@ def load_data(file_path):
     with open(file_path, 'r') as handle:
         return json.load(handle)
     
+
+def get_animals_information():
+    for animal in animals_data:
+        if all(k in animal for k in ['name', 'locations', 'characteristics']) and \
+           all(k in animal['characteristics'] for k in ['diet', 'type']):
+              print(f"Name: {animal['name']}, Diet: {animal['characteristics']['diet']}, Location: {animal['locations']}, Type: {animal['characteristics']['type']}\n")
+        else:
+            continue
+
+
 animals_data = load_data('animals_data.json')
+get_animals_information()
